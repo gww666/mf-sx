5ccf013835ed44a48488b0aa79649c9e82891664
qixingzhi <qixingzhi@DR.COM>
2019-05-27 19:10:12 +0800

    client 准备

---
+++ client/src/store/qxz/index.js
@@ -0,0 +1,15 @@
+export default {
+    namespaced: true,
+    state: {
+        a: 1
+    },
+    getters: {
+        
+    },
+    actions: {
+        
+    },
+    mutations: {
+        
+    }
+}
